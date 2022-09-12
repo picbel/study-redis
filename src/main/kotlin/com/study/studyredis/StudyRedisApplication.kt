2857d34ef5845081b1153ab710df5bfca8b91fe2
Picbel <picbel.gon@gmail.com>
2022-09-12 09:21:41 +0900

    init

---
+++ src/main/kotlin/com/study/studyredis/StudyRedisApplication.kt
@@ -0,0 +1,11 @@
+package com.study.studyredis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyRedisApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyRedisApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/studyredis/StudyRedisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.studyredis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyRedisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
